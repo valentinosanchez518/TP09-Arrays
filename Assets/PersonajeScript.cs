728aab57b0a04cfd290dfdf558a6d3d64c9fcf4d
valentinosanchez518 <141331384+valentinosanchez518@users.noreply.github.com>
2024-06-06 09:07:47 -0300

    Initial commit

---
+++ Assets/PersonajeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeScript : MonoBehaviour
+{
+    public int nivelHambre;
+    public AlimentoScript alimento;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+        if (nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+        AlimentoScript alimento;
+        alimento = other.GetComponent<AlimentoScript>();
+        TomarAlimento(alimento.valorAlimentario);
+
+    }
+}
+
